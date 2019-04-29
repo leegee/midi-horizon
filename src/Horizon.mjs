029cc0bed349dc06154a4df28d088c4c4fec58ce
Lee Goddard <leegee@gmail.com>
2019-04-29 16:08:44 +0200

    write midi

--- src/Horizon.mjs
+++ src/Horizon.mjs
@@ -1,55 +1,104 @@
 const fs = require('fs');
-const tmp = require('tmp');
 const Jimp = require('jimp');
+const MidiWriter = require('midi-writer-js');
 
 const SCALES = {
-    pentatonic: [1, 3, 5, 7, 9]
+    pentatonic: ["A", "C", "D", "E", "G"]
 };
 
 const gmErrHandler = (...args) => console.error(args);
 
 module.exports = class Horizon {
+    /**
+     * 
+     * @param opts {Object} options
+     * @param opts.input {string} Path to input image file.
+     * @param opts.outputMidi {string} Path to output file - defaults in input path with a suffix of `.mid`
+     * @param opts.minVelocity {number=0} Ignore pixels below this intensity
+     * @param opts.octaves {number=7} Number of octaves to use
+     * @param opts.scale {string=pentatonic} Invariable atm
+     * @param opts.x  {number?} Number of time slots (beats?)
+     * @param opts.timeFactor {number} Multiplier for MIDI ticks.
+     * @param opts.contrast {number} Range 0 - 1;
+     */
     constructor(opts = {}) {
-        this.octaves = opts.ocatves || 7;
+        this.minVelocity = opts.minVelocity || 0;
+        this.octaves = opts.octaves || 7;
         this.scale = SCALES[opts.scale || 'pentatonic'];
-        this.staveX = opts.x || 100;
+        this.staveX = opts.x || null;
         this.staveY = this.octaves * this.scale.length;
-        this.outputPath = opts.outputPath || 'output.png';
+        this.outputMidi = opts.outputMidi || opts.input;
+        this.outputMidi = this.outputMidi.replace(/\.mid$/);
         this.input = opts.input;
-        this.px = [...new Array(this.staveX)].map(x => new Array(this.staveY));
+        this.outputImgPath = this.input + '_tmp.png';
+        this.timeFactor = opts.timeFactor || 25;
+        this.contrast = 0.5;
 
         if (!fs.existsSync(this.input)) {
             throw new TypeError('input file does not exist at ' + this.input);
         }
     }
 
-    debug() {
-        console.log(this.img);
-        // this.img.write(this.outputPath, gmErrHandler);
-        console.log(this.outputPath);
-    }
-
     async prepare() {
-        console.log('resize %d x %d', this.staveX, this.staveY);
         this.img = await Jimp.read(this.input);
+        if (this.staveX === null) {
+            this.staveX = this.img.bitmap.width;
+        }
+
+        this.px = [...new Array(this.staveX)].map(x => new Array(this.staveY));
+
         await this.img
-            .contrast(.1)
+            .contrast(this.contrast)
             .greyscale()
             .resize(this.staveX * 2, this.staveY * 2, Jimp.RESIZE_NEAREST_NEIGHBOR)
             .autocrop(.1, false)
             .resize(this.staveX, this.staveY, Jimp.RESIZE_NEAREST_NEIGHBOR)
-            .write(this.outputPath);
+            .invert()
+            .write(this.outputImgPath);
+
+        console.assert(this.staveX === this.img.bitmap.width);
+        console.assert(this.staveY === this.img.bitmap.height);
     };
 
     async getData() {
-        for (let x = 0; x < this.img.bitmap.width; x++) {
-            // High to low
-            for (let y = 0; y < this.img.bitmap.height; y++) {
+        for (let x = 0; x < this.staveX; x++) {
+            for (let y = 0; y < this.staveY; y++) {
                 this.px[x][y] = Jimp.intToRGBA(
                     this.img.getPixelColor(x, y)
                 ).r;
             }
         }
-        console.dir(this.px)
+    }
+
+    createMIDI() {
+        // this.tracks = new Array(this.staveX).fill(new MidiWriter.Track());
+        this.track = new MidiWriter.Track();
+
+        for (let x = 0; x < this.staveX; x++) {
+            const start = (x + 1) * this.timeFactor;
+
+            for (let y = 0; y < this.staveY; y++) {
+                const velocity = (this.px[x][y] / 255) * 100;
+                if (velocity > this.minVelocity) {
+                    const pitch = y % this.scale.length;
+                    const octave = Math.floor(y / this.scale.length) + 1;
+                    this.track.addEvent(
+                        new MidiWriter.NoteEvent({
+                            pitch: this.scale[pitch] + octave,
+                            velocity: velocity,
+                            duration: 'T' + this.timeFactor,
+                            startTick: start
+                        })
+                    );
+
+                }
+            }
+        }
+    }
+
+    saveMIDI() {
+        const write = new MidiWriter.Writer(this.track);
+        write.saveMIDI(this.outputMidi);
+        return this.outputMidi + '.mid';
     }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const chai = require('chai');
 
 const Horizon = require('./Horizon.mjs');
@@ -16,20 +18,19 @@ describe('horizon', () => {
     });
 
     describe('resize', () => {
+        const h = new Horizon({
+            input: './test/london.jpg',
+            x: 100
+        });
+
         it('should resize input image', async () => {
-            const h = new Horizon({
-                input: './test/london.jpg'
-            });
             await h.prepare();
 
-
-            console.log(h.img.bitmap.width, h.staveX);
-            console.log(h.img.bitmap.height, h.staveY);
-
-            
             expect(h.img.bitmap.width).to.equal(h.staveX, 'x');
             expect(h.img.bitmap.height).to.equal(h.staveY, 'y');
+        });
 
+        it('should get image data', async () => {
             await h.getData();
 
             for (let x = 0; x < this.staveX; x++) {
@@ -37,6 +38,19 @@ describe('horizon', () => {
                     expect(h.px[x][y]).not.to.be.undefined;
                 }
             }
+        });
+
+        it('should produce MIDI data', () => {
+            h.createMIDI();
+        });
+
+        it('should save a .mid file', () => {
+            const savedAt = h.saveMIDI();
+            console.log(savedAt);
+            expect(
+                fs.existsSync(
+                    path.resolve(savedAt))
+            ).to.be.true;
         });
     });
 });
